24f42e0df5cfd810ca94c638254016eebf34fc92
Jess B <mcroolz4ever@gmail.com>
2022-06-23 21:26:12 -0500

    Update repository tag

--- example/target/debug/build/compiler_builtins-fc884bf7f8e959ff/build_script_build-fc884bf7f8e959ff.d
+++
@@ -1,5 +0,0 @@
-/home/jbuetow/Projects/Rust/lotus/limine-protocol/example/target/debug/build/compiler_builtins-fc884bf7f8e959ff/build_script_build-fc884bf7f8e959ff: /home/jbuetow/.cargo/registry/src/github.com-1ecc6299db9ec823/compiler_builtins-0.1.73/build.rs
-
-/home/jbuetow/Projects/Rust/lotus/limine-protocol/example/target/debug/build/compiler_builtins-fc884bf7f8e959ff/build_script_build-fc884bf7f8e959ff.d: /home/jbuetow/.cargo/registry/src/github.com-1ecc6299db9ec823/compiler_builtins-0.1.73/build.rs
-
-/home/jbuetow/.cargo/registry/src/github.com-1ecc6299db9ec823/compiler_builtins-0.1.73/build.rs:
--- example/target/x86_64-unknown-none/debug/deps/example_kernel-407cd9edd9b63b74.d
+++
@@ -1,6 +0,0 @@
-/home/jbuetow/Projects/Rust/lotus/limine-protocol/example/target/x86_64-unknown-none/debug/deps/example_kernel-407cd9edd9b63b74.rmeta: src/main.rs src/writer.rs
-
-/home/jbuetow/Projects/Rust/lotus/limine-protocol/example/target/x86_64-unknown-none/debug/deps/example_kernel-407cd9edd9b63b74.d: src/main.rs src/writer.rs
-
-src/main.rs:
-src/writer.rs:
